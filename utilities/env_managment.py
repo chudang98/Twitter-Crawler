eefc33aafcfd71a13d5d7bc8c4c12e2f428fd209
dang.chu <dang.chu@onemount.com>
2023-05-09 12:24:44 +0700

    edit path auth

--- utilities/env_managment.py
+++ utilities/env_managment.py
@@ -2,8 +2,8 @@ import os
 
 PATH_DRIVER = os.getenv('PATH_DRIVER', '/Users/dangchu/Documents/driver/chromedriver_109')
 
-# KEY_PATH = os.getenv('KEY_PATH', '/app/cert/ggsheet_personal.json')
-KEY_PATH = os.getenv('KEY_PATH', '/Users/dangchu/Documents/cert/ggsheet_personal.json')
+KEY_PATH = os.getenv('KEY_PATH', '/app/cert/ggsheet_personal.json')
+# KEY_PATH = os.getenv('KEY_PATH', '/Users/dangchu/Documents/cert/ggsheet_personal.json')
 
 LINK_SHEET_PARSER = os.getenv('LINK_SHEET_PARSER', 'https://docs.google.com/spreadsheets/d/1UKtxXz9e8dJftp-dWi62vNN0vAfoQOCg0zP1TbP4TgA')
 CACHE_DRIVER_LOCATION = os.getenv('CACHE_LOCATION', '/Users/dangchu/Documents/cache')
@@ -12,8 +12,8 @@ PWD_TWITTER = os.getenv('PWD_TWITTER', 'randompass1234')
 CHECKPOINT_SHEET = os.getenv('CHECKPOINT_SHEET', 'Checkpoint Crawler')
 SHEET_DATA = os.getenv('SHEET_DATA', 'https://docs.google.com/spreadsheets/d/1vrDqX94v0NL-rviyKjAeA1hJUlxhGmD28VuJkwNZT5Y/')
 
-SA_AUTH = os.getenv('SA_AUTH', '/Users/dangchu/Documents/GitRepo/secret/cert/canvas-figure-bq.json')
-# SA_AUTH = os.getenv('SA_AUTH', '/app/cert/canvas-figure-bq.json')
+# SA_AUTH = os.getenv('SA_AUTH', '/Users/dangchu/Documents/GitRepo/secret/cert/canvas-figure-bq.json')
+SA_AUTH = os.getenv('SA_AUTH', '/app/cert/canvas-figure-bq.json')
 
 API_TOKEN = os.getenv('API_TOKEN', 'AAAAAAAAAAAAAAAAAAAAAD%2BUkgEAAAAAxuCobQH%2FAcqOzlk6MitTc3vy9no%3DjtTDVRwOEtPhdJEVP1NVIgCtrTIJGAFVDkv7z3UnfV6mvKOlUG')
 
